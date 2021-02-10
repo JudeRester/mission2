32b830d5ba35b8ee29d9c13e9784d6e2cba8e4a0
jude <llkkeor@naver.com>
2021-02-10 16:51:12 +0900

    detail, mod/delete

--- frontend2/src/components/detail/Info.tsx
+++ frontend2/src/components/detail/Info.tsx
@@ -10,53 +10,83 @@ import {
     ListItemText,
     ListItemIcon,
     makeStyles,
+    GridList,
+    GridListTile,
+    GridListTileBar,
 } from "@material-ui/core"
 import { StarBorder, ExpandLess, ExpandMore } from "@material-ui/icons"
 // import { InboxIcon } from '@material-ui/icons/MoveToInbox';
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Carousel from "react-material-ui-carousel"
 import axios from "axios";
 
 
 const useStyles = makeStyles(() =>
-  ({
+({
     primary: {
-      fontSize:'20px',
-      textAlign:'center'
+        fontSize: '20px',
+        textAlign: 'center'
     },
     nested: {
-      paddingLeft: 4,
+        paddingLeft: 4,
     },
-  }),
+}),
 );
 
-type MatchParams = {
-    assetSeq:string
+type Asset = {
+    assetSeq: number,
+    assetOwner: string,
+    assetOwnName: string,
+    assetChanger: string,
+    assetChangerName: string,
+    assetTitle: string,
+    assetCreateDate: Date,
+    assetUpdateDate: Date,
+    tags: string,
+    assetCategory: number,
+    assetCategoryName: string,
+    assetFiles: Array<AssetFile>,
+}
+type AssetFile = {
+    assetSeq: number,
+    assetLocation: string,
+    assetOriginName: string,
+    assetSize: number,
+    assetType: string,
 }
 
-const Info = (props:MatchParams) => {
+type MatchParams = {
+    assetSeq: string
+}
+
+const Info = (props: MatchParams) => {
     const sessionUser = sessionStorage.getItem("sessionUser");
     if (sessionUser) {
         axios.defaults.headers.common['Authorization'] = 'Bearer ' + JSON.parse(sessionUser).token;
     }
 
-    const assetSeq:string = props.assetSeq;
+    const assetSeq: string = props.assetSeq;
     const classes = useStyles();
     const [isOpen, setIsOpen] = useState(false);
+    const [assetInfo, setAssetInfo] = useState<Asset>();
     const handleClick = () => {
         setIsOpen(!isOpen);
-      };
-    
+    };
+
     const items = {
         assetSeq: 6,
         title: "My sixth post",
         excerpt: "This is my sixth post with more content inside",
         image: ["https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml"]
     }
-    axios.get(`/api/asset/${assetSeq}`
-    ).then(result=>{
-        console.log('result',result.data.result)
-    })
+    useEffect(() => {
+        axios.get(`/api/asset/${assetSeq}`
+        ).then(result => {
+            setAssetInfo(result.data.result);
+            console.log(assetInfo)
+        })
+    }, [assetSeq])
+
     return (
         <div style={{ marginTop: 20, padding: 30 }}>
             <Grid container spacing={2}>
@@ -67,11 +97,29 @@ const Info = (props:MatchParams) => {
                         indicators={true}
                         navButtonsAlwaysVisible={true}
                     >
-                        {items.image.map((item, i) => (
+                        {assetInfo && assetInfo.assetFiles.map((item, i) => (
                             <Grid item>
                                 <CardContent>
                                     <CardMedia>
-                                        <img src={item} alt="" />
+                                        {!item.assetType ?
+                                            <div>
+                                                <img src={"/images/unsupportedFile.png"} alt="" />
+                                                <GridListTileBar title={"미리보기가 지원되지 않는 형식"} />
+                                            </div>
+                                            :
+                                            item.assetType.includes("image") ?
+                                                <img style={{ maxWidth: '1000px' }} src={item.assetLocation.substring(item.assetLocation.lastIndexOf("/uploadedImages"))} alt="" />
+                                                : item.assetType.includes("video") ?
+                                                    <video style={{ maxWidth: '1000px' }} controls src={item.assetLocation.substring(item.assetLocation.lastIndexOf("/uploadedImages"))} />
+                                                    :
+                                                    item.assetType.includes("audio") ?
+                                                        <audio src={item.assetLocation.substring(item.assetLocation.lastIndexOf("/uploadedImages"))} />
+                                                        : <div>
+                                                            <img src={"/images/unsupportedFile.png"} alt="" />
+                                                            <GridListTileBar title={"미리보기가 지원되지 않는 형식"} />
+                                                        </div>
+                                        }
+
                                     </CardMedia>
                                 </CardContent>
                             </Grid>
@@ -93,13 +141,13 @@ const Info = (props:MatchParams) => {
                 </Grid>
                 <Grid item xs={12}>
                     <ListItem button onClick={handleClick}>
-                        <ListItemText primary="파일 리스트" classes={{primary:classes.primary}}/>
+                        <ListItemText primary="파일 리스트" classes={{ primary: classes.primary }} />
                         {isOpen ? <ExpandLess /> : <ExpandMore />}
                     </ListItem>
                     <Collapse in={isOpen} timeout="auto" unmountOnExit>
                         <List component="div" disablePadding>
                             <ListItem>
-                                <ListItemText primary="Starred" className={classes.nested}/>
+                                <ListItemText primary="Starred" className={classes.nested} />
                             </ListItem>
                         </List>
                     </Collapse>
--- frontend2/src/components/main/Posts.tsx
+++ frontend2/src/components/main/Posts.tsx
@@ -8,8 +8,12 @@ import {
   GridList,
   GridListTile,
   makeStyles,
-  GridListTileBar
+  GridListTileBar,
+  Paper,
+
 } from '@material-ui/core'
+
+import { Pagination } from '@material-ui/lab';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
@@ -22,128 +26,88 @@ const useStyles = makeStyles(() => ({
   imageCountLessEqualTwo: {
     height: '100%!important'
   },
-  imageCountOverTwo:{
+  imageCountOverTwo: {
     height: '50%!important'
   }
 }))
 
 type Asset = {
-  assetSeq:number,
-  assetOwner:string,
-  assetOwnName:string,
-  assetTitle:string,
-  assetCreateDate:Date,
-  assetUpdateDate:Date,
-  locations:string,
-  tags:string,
-  locationArray:Array<string>
+  assetSeq: number,
+  assetOwner: string,
+  assetOwnName: string,
+  assetTitle: string,
+  assetCreateDate: Date,
+  assetUpdateDate: Date,
+  locations: string,
+  tags: string,
+  locationArray: Array<string>,
+  assetCategory: number,
+  assetCategoryName: string
 }
 
 type Page = {
-  startPage:number,
-  endPage:number,
-  prev:false,
-  next:false,
-  total:number
+  startPage: number,
+  endPage: number,
+  prev: false,
+  next: false,
+  total: number
 }
 
 
 const Posts = () => {
   let sessionUser = sessionStorage.getItem("sessionUser");
-    if (sessionUser) {
-        axios.defaults.headers.common['Authorization'] = 'Bearer ' + JSON.parse(sessionUser).token;
-    }
-  // const contents = [
-
-  //   {
-  //     assetSeq : 1,
-  //     title: "My first post",
-  //     excerpt: "This is my first post with more content inside",
-  //     image: ["https://bit.ly/2WNi2Ml"]
-  //   },
-
-  //   {
-  //     assetSeq : 2,
-  //     title: "My second post",
-  //     excerpt: "This is my second post with more content inside",
-  //     image: ["https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml"]
-  //   },
-
-  //   {
-  //     assetSeq : 3,
-  //     title: "My third post",
-  //     excerpt: "This is my third post with more content inside",
-  //     image: ["https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml"]
-  //   },
-
-  //   {
-  //     assetSeq : 4,
-  //     title: "My fourth post",
-  //     excerpt: "This is my fourth post with more content inside",
-  //     image: ["https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml"]
-  //   },
-
-  //   {
-  //     assetSeq : 5,
-  //     title: "My fifth post",
-  //     excerpt: "This is my fifth post with more content inside",
-  //     image: ["https://bit.ly/2WNi2Ml","https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml"]
-  //   },
-
-  //   {
-  //     assetSeq : 6,
-  //     title: "My sixth post",
-  //     excerpt: "This is my sixth post with more content inside",
-  //     image: ["https://bit.ly/2WNi2Ml","https://bit.ly/2WNi2Ml","https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml", "https://bit.ly/2WNi2Ml"]
-  //   }
-  // ]
+  if (sessionUser) {
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + JSON.parse(sessionUser).token;
+  }
 
   const [contents, setContents] = useState<Array<Asset>>([]);
   const [pageNum, setPageNum] = useState(1);
   const [pageInfo, setPageInfo] = useState<Page>();
   const history = useHistory();
-  const toAssetDetail=(assetSeq:number)=>{
-    history.push('detail/'+assetSeq)
+  const toAssetDetail = (assetSeq: number) => {
+    history.push('detail/' + assetSeq)
   }
   const classes = useStyles();
-  async function loadContents(){
+  async function loadContents() {
     sessionUser = sessionStorage.getItem("sessionUser");
     if (sessionUser) {
-        axios.defaults.headers.common['Authorization'] = 'Bearer ' + JSON.parse(sessionUser).token;
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + JSON.parse(sessionUser).token;
     }
     const response = await axios.get(`/api/list/${pageNum}`)
-    const list:Array<Asset> = response.data.result
+    const list: Array<Asset> = response.data.result
     setPageInfo(response.data.reference)
     setContents(list)
     console.log(contents, pageInfo);
   };
-  useEffect(()=>{
+  useEffect(() => {
     loadContents();
-  },[pageNum]);
-  
+  }, [pageNum]);
+
   return (
-    <div style={{ marginTop: 20, padding: 30 }}>
-      <Grid container spacing={3} justify="center">
-        {contents&& contents.map(post => (
-          <Grid item key={post.assetSeq}>
-            <Card>
+    <div style={{ 
+      // marginTop: 20, 
+      padding: 30 }}>
+        <Grid container spacing={3} justify="center">
+          {contents && contents.map(post => (
+            <Grid item key={post.assetSeq}>
+              <Card>
                 <GridList className={classes.gridList}>
                   {
                     post.locationArray.map((imgUrl, i) => (
                       (post.locationArray.length < 3) ?
                         <GridListTile key={i} cols={(post.locationArray.length % 2 != 0 && i == 0) ? 2 : 1} classes={{ root: classes.imageCountLessEqualTwo }}>
-                          <img src={'/uploadedImages/thumb'+ imgUrl.substring(imgUrl.lastIndexOf("/uploadedImages") + 15)} alt="" />
+                          <img src={'/uploadedImages/thumb' + imgUrl.substring(imgUrl.lastIndexOf("/uploadedImages") + 15)} alt="" />
 
                         </GridListTile>
-                        : 
-                        i<=3?
-                        <GridListTile key={i} cols={(post.locationArray.length % 2 != 0 && i == 0 && post.locationArray.length<=3) ? 2 : 1} classes={{root:classes.imageCountOverTwo}}>
-                          <img src={'/uploadedImages/thumb'+ imgUrl.substring(imgUrl.lastIndexOf("/uploadedImages") + 15)} alt="" />
-                          {(post.locationArray.length>4 && i==3)?
-                          <GridListTileBar title={post.locationArray.length-(i+1)+'개 더보기'} classes={{root:classes.imageCountLessEqualTwo}}/>
-                          : null}
-                        </GridListTile>:
-                        null
+                        :
+                        i <= 3 ?
+                          <GridListTile key={i} cols={(post.locationArray.length % 2 != 0 && i == 0 && post.locationArray.length <= 3) ? 2 : 1} classes={{ root: classes.imageCountOverTwo }}>
+                            <img src={'/uploadedImages/thumb' + imgUrl.substring(imgUrl.lastIndexOf("/uploadedImages") + 15)} alt="" />
+                            {(post.locationArray.length > 4 && i == 3) ?
+                              <GridListTileBar title={post.locationArray.length - (i + 1) + '개 더보기'} classes={{ root: classes.imageCountLessEqualTwo }} />
+                              : null}
+                          </GridListTile> :
+                          null
                     ))
                   }
                 </GridList>
@@ -154,16 +118,16 @@ const Posts = () => {
                   </Typography>
                   <Typography component="p">{post.tags}</Typography>
                 </CardContent>
-              <CardActions>
-                <Button size="small" color="primary" onClick={()=>{toAssetDetail(post.assetSeq)}}>
-                  상세보기
+                <CardActions>
+                  <Button size="small" color="primary" onClick={() => { toAssetDetail(post.assetSeq) }}>
+                    상세보기
                 </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        )) 
-        }
-      </Grid>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))
+          }
+        </Grid>
     </div>
   )
 }
